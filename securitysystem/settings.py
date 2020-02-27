7cb82233fbf7d078944559fb950c2649f4be2a8e
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2020-02-27 19:06:16 +0300

    addition of template

--- securitysystem/settings.py
+++ securitysystem/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'securitysystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # user model
 AUTH_USER_MODEL = 'user.User'
