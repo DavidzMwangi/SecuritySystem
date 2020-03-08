11ec09222e9fed2742fe4edbcc22802325a6129f
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2020-03-08 15:15:30 +0300

    completed setting up of the template

--- securitysystem/urls.py
+++ securitysystem/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('', TemplateView.as_view(template_name='base.html')),
+    path('', TemplateView.as_view(template_name='login.html')),
 
 ]
